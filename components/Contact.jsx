d914b3f245c9206df9edc7dcfa2b3916138abc6d
Mohamed Sonbol <mhmdezzat.eng@gmail.com>
2023-03-06 14:04:05 +0100

    push for deployment

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -4,7 +4,7 @@ import Hero from './Hero'
 function Contact() {
   return (
     <div className='max-w-[870px] m-auto p-4 h-screen'>
-        <h1 className='text-2xl font-bold text-center p-4'> Let's work together</h1>
+        <h1 className='text-2xl font-bold text-center p-4'> Let us work together</h1>
         <form className='max-w-[600] m-auto' action="">
             <div className='grid grid-cols-2 gap-2'>
                 <input className='border shadow-lg p-3' type="text" placeholder='Name' />
