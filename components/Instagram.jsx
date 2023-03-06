510bec9dfda52ba29752ccd4a4acd31afdfe328e
Mohamed Sonbol <mhmdezzat.eng@gmail.com>
2023-03-06 14:07:01 +0100

    push for deployment

--- components/Instagram.jsx
+++ components/Instagram.jsx
@@ -11,7 +11,7 @@ const Instagram = () => {
   return (
     <div className='max-w-[1240px] mx-auto text-center py-24'>
         <p className='text-2xl font-bold'>Follow me on Instagram</p>
-        <p className='pb-4'>@rastezzy</p>
+        <p className='pb-4'>@instagram</p>
         <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-2 p-4'>
             <InstagramImg socialImg={IgImg1} />
             <InstagramImg socialImg={IgImg2} />
